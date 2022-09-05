0e7f59df573894d9e9405804c58c009b4eee0f32
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-09-05 23:53:13 +0000

    Created Connection to Broker Feature

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const connectRouter = require('./routes/connect');
+
+// parse the incoming requests with JSON payloads 
+app.use(express.json());
+
+// set the port 
+const port = process.env.PORT || 8080;
+
+// routers
+app.use('/broker', connectRouter);
+
+
+// the server will be listening on port
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+ });
