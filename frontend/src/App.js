224fc909ca724f46b0ab6810105c05e2ba2ca8ac
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-05 22:01:47 +0000

    Import and render connection settings page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./css/normalize.css";
 import "./css/skeleton.css";
 
 import Dashboard from "./pages/Dashboard/Dashboard";
+import ConnectionSettings from "./pages/ConnectionSettings/ConnectionSettings";
 
 function App() {
   return (
     <div>
       <Dashboard />
+      <ConnectionSettings />
     </div>
   );
 }
