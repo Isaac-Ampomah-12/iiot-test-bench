74e0d3911051481bff17e564e53b2abb4733d448
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-05 23:44:13 +0000

    Add class info container to metric column

--- frontend/src/components/Broker/BrokerMetrics/BrokerMetrics.js
+++ frontend/src/components/Broker/BrokerMetrics/BrokerMetrics.js
@@ -7,17 +7,19 @@ function BrokerMetrics() {
         <div className="row">
           <div className="four columns">
             <h3>Connection</h3>
-            <div>
-              <span><strong>Host: </strong></span>
-              <span>broker.hivemq.com</span>
-            </div>
-            <div>
-              <span><strong>Port: </strong></span>
-              <span>8080</span>
-            </div>
-            <div>
-              <span><strong>Status: </strong></span>
-              <span>Disconnected</span>
+            <div className="info">
+              <div>
+                <span><strong>Host: </strong></span>
+                <span>broker.hivemq.com</span>
+              </div>
+              <div>
+                <span><strong>Port: </strong></span>
+                <span>8080</span>
+              </div>
+              <div>
+                <span><strong>Status: </strong></span>
+                <span>Disconnected</span>
+              </div>
             </div>
           </div>
         </div>
