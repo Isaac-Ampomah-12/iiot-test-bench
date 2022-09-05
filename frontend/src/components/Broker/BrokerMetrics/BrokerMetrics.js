5fe8bf283fc19cc38b34da87b2435b7f9767cd26
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-05 23:57:41 +0000

    Add messages metric column

--- frontend/src/components/Broker/BrokerMetrics/BrokerMetrics.js
+++ frontend/src/components/Broker/BrokerMetrics/BrokerMetrics.js
@@ -9,16 +9,39 @@ function BrokerMetrics() {
             <h3>Connection</h3>
             <div className="info">
               <div>
-                <span><strong>Host: </strong></span>
+                <span>
+                  <strong>Host: </strong>
+                </span>
                 <span>broker.hivemq.com</span>
               </div>
               <div>
-                <span><strong>Port: </strong></span>
+                <span>
+                  <strong>Port: </strong>
+                </span>
                 <span>8080</span>
               </div>
               <div>
-                <span><strong>Status: </strong></span>
+                <span>
+                  <strong>Status: </strong>
+                </span>
                 <span>Disconnected</span>
+              </div>
+            </div>
+          </div>
+          <div className="three columns">
+            <h3>Messages</h3>
+            <div class="info">
+              <div>
+                <span>
+                  <strong>Sent: </strong>
+                </span>
+                <span>0</span>
+              </div>
+              <div>
+                <span>
+                  <strong>Received: </strong>
+                </span>
+                <span>0</span>
               </div>
             </div>
           </div>
