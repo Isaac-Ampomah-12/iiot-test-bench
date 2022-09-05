956f41722c09b653e6b9d8e78bd282e6e998aacd
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-05 21:52:26 +0000

    Move Dashboard page component to pages folder

---
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import BrokerMetrics from "../../components/Broker/BrokerMetrics/BrokerMetrics";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>IIoT Test Bench</h1>
+      <BrokerMetrics />
+    </div>
+  );
+}
+
+export default Dashboard;
