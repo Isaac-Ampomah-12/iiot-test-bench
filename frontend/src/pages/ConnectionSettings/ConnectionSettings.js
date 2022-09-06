7804d6c7ad13c4bdd930fb55d52826b3a085dc31
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-06 09:54:10 +0000

    Add host, port and client id inputs

--- frontend/src/pages/ConnectionSettings/ConnectionSettings.js
+++ frontend/src/pages/ConnectionSettings/ConnectionSettings.js
@@ -5,6 +5,42 @@ function ConnectionSettings() {
     <div className="container">
       <section id="Connect-settings">
         <h2>Connection settings</h2>
+        <form>
+          <div className="row">
+            <div className="five columns">
+              <label htmlFor="host">Host:</label>
+              <input
+                type="url"
+                id="host"
+                className="u-full-width"
+                placeholder="E.g. mqtt://broker.hostname.com"
+                required
+                autofocus
+              />
+            </div>
+            <div className="two columns">
+              <label htmlFor="port">Port:</label>
+              <input
+                type="number"
+                id="port"
+                className="u-full-width"
+                min="0"
+                value="1883"
+                required
+              />
+            </div>
+            <div className="five columns">
+              <label htmlFor="port">Client ID:</label>
+              <input
+                type="text"
+                id="client-id"
+                className="u-full-width"
+                value="clientId-XXXXXXXXXX"
+                required
+              />
+            </div>
+          </div>
+        </form>
       </section>
     </div>
   );
