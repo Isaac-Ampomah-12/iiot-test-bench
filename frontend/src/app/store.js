f8857f4e6bfb397ed68746cbfbe5fe616395d960
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-07 01:21:53 +0000

    Fix connectionSlice import error

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { connectionSlice } from "./connectionSlice";
+import connectionSlice from "./connectionSlice";
 
 export const store = configureStore({
   reducer: {
