5a4fa4a8a4c65a588d51bc4a8d3227eb86801718
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 09:16:02 +0000

    Add number of publishers input and label

--- frontend/src/components/Publisher/Publisher.js
+++ frontend/src/components/Publisher/Publisher.js
@@ -3,7 +3,19 @@ function Publisher() {
     <section id="publisher">
       <h2>Publisher</h2>
       <form id="publisher-settings">
-        <div class="row">
+        <div className="row">
+          <div className="four columns">
+            <div>
+              <label htmlFor="no-of-pub">No. of publishers:</label>
+              <input
+                type="number"
+                id="no-of-pub"
+                name="publisherNumber"
+                defaultValue="0"
+                min="0"
+              />
+            </div>
+          </div>
         </div>
       </form>
     </section>
