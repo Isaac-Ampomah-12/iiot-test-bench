c95e81f094698877bfe566f11de085ca1d920516
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 19:55:18 +0000

    Add number of subscribers input with label

--- frontend/src/components/Subscriber/Subscriber.js
+++ frontend/src/components/Subscriber/Subscriber.js
@@ -2,6 +2,14 @@ function Subscriber() {
   return (
     <section>
       <h2>Subscriber</h2>
+      <div className="row">
+        <div className="four columns">
+          <div>
+            <label for="no-of-sub">No. of subscribers:</label>
+            <input type="number" id="no-of-sub" defaultValue="0" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
