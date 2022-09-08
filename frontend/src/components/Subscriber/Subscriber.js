620b6b78907ab594b53ce715f742089a2f5af6f6
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 20:03:27 +0000

    Add state variable and onChange event handler for number of subscribers

--- frontend/src/components/Subscriber/Subscriber.js
+++ frontend/src/components/Subscriber/Subscriber.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 function Subscriber() {
+  const [subNumber, setSubNumber] = useState(0);
+  
   return (
     <section>
       <h2>Subscriber</h2>
       <div className="row">
         <div className="four columns">
           <div>
-            <label for="no-of-sub">No. of subscribers:</label>
-            <input type="number" id="no-of-sub" defaultValue="0" />
+            <label htmlFor="no-of-sub">No. of subscribers:</label>
+            <input
+              type="number"
+              id="no-of-sub"
+              value={subNumber}
+              onChange={({target}) => setSubNumber(target.value)}
+            />
           </div>
         </div>
       </div>
