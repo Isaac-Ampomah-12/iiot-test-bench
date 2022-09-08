cc895aeb6cf21a9d53c11b1bd9b1b869640595f5
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 22:19:34 +0000

    Add slice name, initial state and reducers

--- frontend/src/app/publishSlice.js
+++ frontend/src/app/publishSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const publishSlice = createSlice({
-
+  name: 'publish',
+  initialState: {
+    settings: {},
+    stats: {
+      performance: { cpu: 0, memory: 0 }
+    }
+  },
+  reducers: {}
 });
 
 export default publishSlice.reducer;
