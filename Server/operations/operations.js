7bc69ca87540f7b30a52639c1408e281ae538269
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-09-08 23:42:01 +0000

    Created function to generate random topic based on the publish topic level the user sets

--- Server/operations/operations.js
+++ Server/operations/operations.js
@@ -1,5 +1,20 @@
+// import random-words
+var randomWords = require('random-words');
+
 // this object will hold methods
 const operations = {
+    
+    // this method will generate a random topic depending on the topic level the user sets
+    generateTopic (topicLevel) {
+        // create an array of random word and the array length is dependent on the topic level 
+        let words = randomWords(topicLevel);
+        
+        // create a string of words seperated with "/"
+        let topic = words.join('/');
+
+        // return the topic created
+        return topic
+    },
 
     // this method will generate a random message depending on the message size the use sets
     generateMessage (num) {
