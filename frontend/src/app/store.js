2b6ed2bfcc0278a9ec678f9388b9fa986ece7554
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 22:20:59 +0000

    Import and add publishSlice to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import connectionSlice from "./connectionSlice";
+import publishSlice from "./publishSlice";
 
 export const store = configureStore({
   reducer: {
     connection: connectionSlice,
+    publish: publishSlice
   },
 });
