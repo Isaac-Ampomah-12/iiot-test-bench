03a182646b07a25cdced99a519f2dd8dc9b3acb8
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 20:34:12 +0000

    Remove form element

--- frontend/src/components/Publisher/Publisher.js
+++ frontend/src/components/Publisher/Publisher.js
@@ -8,83 +8,81 @@ function Publisher() {
   return (
     <section id="publisher">
       <h2>Publisher</h2>
-      <form id="publisher-settings">
-        <div className="row">
-          <div className="three columns">
-            <label htmlFor="no-of-pub">No. of publishers:</label>
+      <div className="row">
+        <div className="three columns">
+          <label htmlFor="no-of-pub">No. of publishers:</label>
+          <input
+            type="number"
+            id="no-of-pub"
+            className="u-full-width"
+            name="publisherNumber"
+            min="0"
+            value={pubNumber}
+            onChange={({ target }) => setPubNumber(target.value)}
+          />
+        </div>
+        <div className="three columns">
+          <div>
+            <label htmlFor="topic-levels">Topic Levels:</label>
             <input
               type="number"
-              id="no-of-pub"
+              id="topic-levels"
               className="u-full-width"
-              name="publisherNumber"
               min="0"
-              value={pubNumber}
-              onChange={({ target }) => setPubNumber(target.value)}
+              value={pubTopicLevel}
+              onChange={({ target }) => setPubTopicLevel(target.value)}
             />
           </div>
-          <div className="three columns">
-            <div>
-              <label htmlFor="topic-levels">Topic Levels:</label>
-              <input
-                type="number"
-                id="topic-levels"
-                className="u-full-width"
-                min="0"
-                value={pubTopicLevel}
-                onChange={({ target }) => setPubTopicLevel(target.value)}
-              />
-            </div>
+        </div>
+        <div className="three columns">
+          <label htmlFor="interval" style={{ display: "inline" }}>
+            Interval &#40;ms&#41;:
+          </label>
+          <output
+            htmlFor="interval"
+            style={{ display: "inline", paddingLeft: "1rem" }}
+          >
+            {pubInterval}
+          </output>
+          <div
+            style={{
+              marginTop: "1rem",
+              display: "flex",
+              justityContent: "center",
+            }}
+          >
+            <input
+              type="range"
+              id="interval"
+              className="u-full-width"
+              min="0"
+              max="10000"
+              step="100"
+              title={pubInterval}
+              value={pubInterval}
+              onChange={({ target }) => setPubInterval(target.value)}
+              style={{ marginBottom: "0" }}
+            />
           </div>
-          <div className="three columns">
-            <label htmlFor="interval" style={{ display: "inline" }}>
-              Interval &#40;ms&#41;:
-            </label>
-            <output
-              htmlFor="interval"
-              style={{ display: "inline", paddingLeft: "1rem" }}
-            >
-              {pubInterval}
-            </output>
-            <div
-              style={{
-                marginTop: "1rem",
-                display: "flex",
-                justityContent: "center",
-              }}
-            >
-              <input
-                type="range"
-                id="interval"
-                className="u-full-width"
-                min="0"
-                max="10000"
-                step="100"
-                title={pubInterval}
-                value={pubInterval}
-                onChange={({ target }) => setPubInterval(target.value)}
-                style={{ marginBottom: "0" }}
-              />
+        </div>
+        <div className="three columns">
+          <h3>Performance</h3>
+          <div className="info">
+            <div className="info-item">
+              <span>
+                <strong>CPU:</strong>
+              </span>
+              <span>0</span>
             </div>
-          </div>
-          <div className="three columns">
-            <h3>Performance</h3>
-            <div className="info">
-              <div className="info-item">
-                <span>
-                  <strong>CPU:</strong>
-                </span>
-                <span>0</span>
-              </div>
-              <div className="info-item">
-                <span>
-                  <strong>Memory:</strong>
-                </span>
-                <span>0</span>
-              </div>
+            <div className="info-item">
+              <span>
+                <strong>Memory:</strong>
+              </span>
+              <span>0</span>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
