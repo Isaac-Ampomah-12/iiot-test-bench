98776f739384be8438294e8e6720fa4f1fc57ae8
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-09-08 22:47:52 +0000

    created integration test for server.js

--- Server/test/server_test.js
+++ Server/test/server_test.js
@@ -1,39 +1,75 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 // const { options } = require('../server');
+var should = chai.should();
 const assert = chai.assert;
 
 const app = require('../server');
 chai.use(chaiHttp);
 
 describe('Configure and test', () => {
-    //     // describe('Connection to Broker', () => {
-        it('connection to broker', (done) => {
-            var clusterInfo = {
-                "host": "f6a5b7010eb4472aa8657f1931c2f8f7.s1.eu.hivemq.cloud",
-                "port": 8883,
-                "protocol": "mqtts",
-                "username": "Isaac Ampomah",
-                "password": "1password@most",
-                // "keepalive": 90,
-                "reconnectPeriod": 0,
-                "clientId": 'mqttjs_df807ed6',
-                "clean": false,
-            }
+    it('connection to broker', (done) => {
+        options = {
+            "host": "f6a5b7010eb4472aa8657f1931c2f8f7.s1.eu.hivemq.cloud",
+            "port": 8883,
+            "protocol": "mqtts",
+            "username": "Isaac Ampomah",
+            "password": "1password@most",
+            "reconnectPeriod": 0
+        }
+
+        var expected = true;
+
+        chai
+            .request(app)
+            .post('/broker/connect')
+            .send(options)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .end((err,res) => {
+                res.should.have.status(200);
+                assert.equal(res.body.connectionStatus, expected);
+                done();
+            })
+    });
     
-    
-            var expected = true;
-    
-            chai
-                .request(app)
-                .post('/broker/connect')
-                .send(clusterInfo)
-                .set('Content-Type', 'application/json')
-                .set('Accept', 'application/json')
-                .end((err,res) => {
-                    assert.equal(res.body.connectionStatus, expected);
-                    done();
-                })
+});
+
+describe('Set', () => {
+    describe('number of publishers that will publish messages to the broker.', () => {
+        describe('number of subscribers that will subscribe to a topic', () => {
+            describe('number of subscribers that will subscribe to a topic', () => {
+                describe('topic level all publishers should publish messages to', () => {
+                    describe('topic level all subscribers should subscribe to', () => {
+                        describe('topic level all subscribers should subscribe to', () => {
+                            describe('configure the size of the message sent by publishers', () => {
+                                it('publish and subscribe', (done) => {
+
+                                    let userInput = {
+                                        "numberOfPublishers": 8,
+                                        "publishTopicLevel": 4,
+                                        "numberOfSubscribers": 8,
+                                        "subscriptionTopicLevel": 4,
+                                        "messageSize": 70
+                                    }
+
+                                    chai
+                                    .request(app)
+                                    .post('/pubsub')
+                                    .send(userInput)
+                                    .set('Content-Type', 'application/json')
+                                    .set('Accept', 'application/json')
+                                    .end((err,res) => {
+                                        res.should.have.status(200);
+                                        // assert.equal(res.body.connectionStatus, expected);
+                                        done();
+                                    })
+                                })
+                            });
+                        })    
+                    })
+                })            
+            })
         });
-    
+    });
 });
