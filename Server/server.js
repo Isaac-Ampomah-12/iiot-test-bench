49c2207fba56487246310bd5868eae17926b6267
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-09-08 23:08:50 +0000

    implemented publish and subscribe

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
-
 const express = require('express');
 const app = express();
+const mqtt = require('mqtt');
 const connectRouter = require('./routes/connect');
 
 const cors = require("cors");
@@ -18,6 +18,32 @@ const port = process.env.PORT || 8080;
 
 // routers
 app.use('/broker', connectRouter);
+
+app.post("/pubsub", (req, res) => {
+
+    var client = mqtt.connect(options);
+    
+    // setup the callbacks
+    client.on('connect', function () {
+        console.log('Connected');
+    });
+
+    client.on('error', function (error) {
+        console.log(error);
+    });
+
+    client.on('message', function (topic, message) {
+        // called each time a message is received
+        console.log('Received message:', topic, message.toString());
+        res.send("passed");
+    });
+
+    // subscribe to topic 'my/test/topic'
+    client.subscribe('my/test/topic');
+
+    // publish message 'Hello' to topic 'my/test/topic'
+    client.publish('my/test/topic', 'Hello');
+});
 
 
 // the server will be listening on port
