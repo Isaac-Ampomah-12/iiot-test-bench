03e8e5daad93abba353a103f316469f41d2fbe5d
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-09-08 23:00:20 +0000

    updated connect.js to stop using global variable

---
+++ Server/routes/connect.js
@@ -0,0 +1,37 @@
+// import express
+const express = require('express');
+
+// import mqtt library
+var mqtt = require('mqtt')
+
+// create a router
+const connectRouter = express.Router();
+
+connectRouter.post('/connect',(req, res) => {
+    let options = req.body;
+    
+    // attempts to connect to broker
+    let brokerClient = mqtt.connect(options);
+
+    // if the connection is successful this event will be triggered
+    brokerClient.on("connect", () => {
+        // this variable will hold true 
+        let connectionStatus = brokerClient.connected
+
+
+        // a true value will be sent to the front end
+        res.send({connectionStatus});
+    });
+
+    //if the connection is unsuccessful this event will be triggered
+    brokerClient.on("error", (error) => {
+        // this variable will hold a false value 
+        let connectionStatus = brokerClient.connected;
+        // a false value will be sent to the front end
+        res.send({connectionStatus});
+    });
+
+});
+
+// this will export the connectRouter
+module.exports = connectRouter;
