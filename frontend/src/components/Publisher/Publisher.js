6ec200914c080a881e177636b33074c27a97533d
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-08 10:44:43 +0000

    Add state variable and onChange event to control publishers number input

--- frontend/src/components/Publisher/Publisher.js
+++ frontend/src/components/Publisher/Publisher.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Publisher() {
+  const [pubNumber, setPubNumber] = useState(0);
+  
   return (
     <section id="publisher">
       <h2>Publisher</h2>
@@ -10,9 +14,11 @@ function Publisher() {
               <input
                 type="number"
                 id="no-of-pub"
+                className="u-full-width"
                 name="publisherNumber"
-                defaultValue="0"
                 min="0"
+                value={pubNumber}
+                onChange={({target}) => setPubNumber(target.value)}
               />
             </div>
           </div>
