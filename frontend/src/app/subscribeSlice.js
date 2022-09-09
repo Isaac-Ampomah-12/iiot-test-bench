c8cd68cfaef0fbf7e7ecf9626441f2614145e7bf
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-09 11:28:22 +0000

    Add name, initialState and reducers propterties

--- frontend/src/app/subscribeSlice.js
+++ frontend/src/app/subscribeSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const subscribeSlice = createSlice({
-
+  name: 'subscribe',
+  initialState: {
+    settings: {},
+    stats: { 
+      performance: { cpu: 0, memomry: 0 }
+    }
+  },
+  reducers: {}
 });
 
 export default subscribeSlice.reducer;
