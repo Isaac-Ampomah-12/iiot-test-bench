9dcd19c58859b4729fba76b994189c1cfd2a5337
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-09 10:23:54 +0000

    Update slice variable, name and initial state

--- frontend/src/app/connectionSlice.js
+++ frontend/src/app/connectionSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const connectionSlice = createSlice({
-    name: "connection",
-    initialState: {
-        settings: {},
-        status: false
+const brokerSlice = createSlice({
+  name: "broker",
+  initialState: {
+    settings: {},
+    stats: {
+      messages: { sent: 0, received: 0 },
+      network: { uplink: 0, downlink: 0 },
+      performance: { cpu: 0, memory: 0 }
     },
-    reducers: {}
+    connection: { 
+      status: false,
+      msg: 'Not connected',
+      color: ''
+    }
+  },
+  reducers: {}
 });
 
-export default connectionSlice.reducer;
+export default brokerSlice.reducer;
