6b77ffd80be97d084cae9de0da5385030296a52c
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-10 19:49:09 +0000

    Update connectToBroker function to use api url environment variable

--- frontend/src/app/actions/brokerActions.js
+++ frontend/src/app/actions/brokerActions.js
@@ -1,10 +1,11 @@
 export function connectToBroker(settings) {
-  const response = fetch(`${process.env.API_URL}/broker/connect`, {
+  const response = fetch(`${process.env.REACT_APP_API_URL}/broker/connect`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(settings),
   });
+  console.log(process.env.REACT_APP_API_URL);
   return response;
 }
