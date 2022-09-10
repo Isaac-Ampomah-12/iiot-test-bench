bcb60414810186ceb6bb9a6a7a8b6e8b4254eff9
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-10 19:56:33 +0000

    Add error handling

--- frontend/src/app/actions/brokerActions.js
+++ frontend/src/app/actions/brokerActions.js
@@ -1,11 +1,14 @@
 export function connectToBroker(settings) {
-  const response = fetch(`${process.env.REACT_APP_API_URL}/broker/connect`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(settings),
-  });
-  console.log(process.env.REACT_APP_API_URL);
-  return response;
+  try {
+    const response = fetch(`${process.env.REACT_APP_API_URL}/broker/connect`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(settings)
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
