941fb4fc48b3a7f1bd833c431df02ac33b5b07fc
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-11 21:29:03 +0000

    Add pubsub slice to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import brokerSlice from "./slices/brokerSlice";
 import publishSlice from "./slices/publishSlice";
 import subscribeSlice from "./slices/subscribeSlice";
+import pubSubSlice from "./slices/pubSubSlice";
 
 export const store = configureStore({
   reducer: {
     broker: brokerSlice,
+    pubsub: pubSubSlice,
     publish: publishSlice,
     subscribe: subscribeSlice
   },
