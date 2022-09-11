5afba956419f4a2dbb6de5330f0d83561b064d07
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-09-11 21:33:19 +0000

    Add setSettings reducer

--- frontend/src/app/slices/pubSubSlice.js
+++ frontend/src/app/slices/pubSubSlice.js
@@ -1,9 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const pubSubSlice = createSlice({
-    name: 'pubSub',
-    initialState: {},
-    reducers: {}
+  name: "pubSub",
+  initialState: {
+    settings: {},
+  },
+  reducers: {
+    setSettings(state, action) {
+      const settings = action.payload;
+      for (const property in settings) {
+        state.settings[property] = Number(settings[property]);
+      }
+    },
+  },
 });
 
+export const { setSettings } = pubSubSlice.actions;
 export default pubSubSlice.reducer;
